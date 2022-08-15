dec23938658d03a143f41d39b4cc81377dd2e946
John Carruthers <johnc@frag-labs.com>
2022-08-15 16:27:10 -0500

    Initial commit.

---
+++ ItemsControlStackOverflowReproduction/ItemsControlStackOverflowReproduction/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ItemsControlStackOverflowReproduction.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string[] Values { get; } = new[]
+        {
+            "Hello",
+            "World"
+        };
+    }
+}
---
+++ ItemsControlStackOverflowReproduction/ItemsControlStackOverflowReproduction/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace ItemsControlStackOverflowReproduction.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
